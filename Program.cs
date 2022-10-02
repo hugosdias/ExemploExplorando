ed8c48436fddba450a895fa284c2b34681cd7878
Hugo S. Dias <hugo_sdias@hotmail.com>
2022-10-02 09:01:46 -0300

    primeiro envio

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa(nome: "Hugo", sobrenome: "Sanguino Dias");
+Pessoa p2 = new Pessoa(nome: "Leticia", sobrenome: "Veloso");
+
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
